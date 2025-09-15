9b6a125f3f15ebddfcf8c2e8490c6b0bbb195f57
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-09-15 17:52:36 +0200

    migrated to v5 api

--- library/src/main/java/com/github/pfichtner/heapwatch/library/StatsReader.java
+++ library/src/main/java/com/github/pfichtner/heapwatch/library/StatsReader.java
@@ -1,13 +1,17 @@
 package com.github.pfichtner.heapwatch.library;
 
 import static com.github.pfichtner.heapwatch.library.acl.Memory.adapt;
+import static java.util.stream.Collectors.toList;
 import static org.eclipselabs.garbagecat.util.Constants.DEFAULT_BOTTLENECK_THROUGHPUT_THRESHOLD;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 import org.eclipselabs.garbagecat.domain.JvmRun;
 import org.eclipselabs.garbagecat.service.GcManager;
-import org.eclipselabs.garbagecat.util.jdk.Jvm;
 
 import com.github.pfichtner.heapwatch.library.acl.Stats;
 
@@ -19,18 +23,26 @@ public final class StatsReader {
 
 	public static Stats stats(File logFile) {
 		GcManager gcManager = new GcManager();
-		gcManager.store(logFile, true);
-		return map(gcManager.getJvmRun(new Jvm(null, null), DEFAULT_BOTTLENECK_THROUGHPUT_THRESHOLD));
+		gcManager.store(readFile(logFile), true);
+		return map(gcManager.getJvmRun(null, DEFAULT_BOTTLENECK_THROUGHPUT_THRESHOLD));
+	}
+
+	private static List<String> readFile(File file) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			return reader.lines().collect(toList());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	private static Stats map(JvmRun jvmRun) {
 		Stats result = new Stats();
 		result.maxHeapAfterGC = adapt(jvmRun.getMaxHeapAfterGc());
 		result.maxHeapOccupancy = adapt(jvmRun.getMaxHeapOccupancy());
-		result.maxHeapSpace = adapt(jvmRun.getMaxHeapSpace());
-		result.maxMetaspaceAfterGC = adapt(jvmRun.getMaxPermAfterGc());
-		result.maxMetaspaceOccupancy = adapt(jvmRun.getMaxPermOccupancy());
-		result.maxMetaspaceSpace = adapt(jvmRun.getMaxPermSpace());
+		result.maxHeapSpace = adapt(jvmRun.getMaxHeap());
+		result.maxMetaspaceAfterGC = adapt(jvmRun.getMaxClassSpaceAfterGc());
+		result.maxMetaspaceOccupancy = adapt(jvmRun.getMaxClassSpaceOccupancy());
+		result.maxMetaspaceSpace = adapt(jvmRun.getMaxClassSpace());
 		return result;
 	}
 
